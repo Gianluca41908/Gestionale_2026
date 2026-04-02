f2eac9649c87a36e877bede604c38077634e75f0
Gianluca41908 <gianluca.padolecchia@gmail.com>
2026-04-02 15:42:39 +0200

    Preparazione backend per deploy

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,18 @@ const practiceRoutes = require('./routes/practiceRoutes');
 
 const app = express()
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors())
 app.use(express.json())
 
 app.use('/clienti', clientRoutes)
 app.use(practiceRoutes);
 
-app.listen(5000, () => {
-    console.log("Server avviato su http://localhost:5000")
-})
+// app.listen(5000, () => {
+//     console.log("Server avviato su http://localhost:5000")
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server avviato sulla porta ${PORT}`);
+});
